f1c496a468a0b60bf30d93acf2923b66ec99518d
Qudamah Coleman <cfcinc.88#gmail.com>
2020-06-03 12:05:17 -0500

    var game = (function(){   var canvas = document.getElementById('canvas');   var ctx = canvas.getContext('2d');
      return {
        init: function(){
          canvas.height = 600;
          canvas.width = 800;
        }
      }

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
-canvas {
-    background: #222;
-  }
+var game = (function(){
+    var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+  
+    return {
+      init: function(){
+        canvas.height = 600;
+        canvas.width = 800;
+      }
+    }
+  })();
+  
+  game.init();
+  
